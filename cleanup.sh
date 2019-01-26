9b6677968bf9ea99c475db0fab200196b1899c15
flaviostutz <flaviostutz@gmail.com>
2019-01-26 00:22:59 -0200

    removing dead code

--- cleanup.sh
+++ cleanup.sh
@@ -17,35 +17,3 @@ echo "CLEANUP END"
 echo "START REGISTRY"
 echo "/entrypoint.sh $CONFIG_PATH" > /async-cmd
 
-
-# SEEMS LIKE LATEST VERSIONS OF docker registry PERFORMS THIS AS WELL. TEST AND COMPARE WITH A FULL REPO
-# IFS=$'\n'
-# used_hashes=`mktemp`
-# marked_hashes=`mktemp`
-# found=""
-# #for repository in `find ${BASE_PATH} -mindepth 2 -maxdepth 2 -type d | sed "s#${BASE_PATH}/##"`; do
-# for repository in `find ${BASE_PATH} -name "_manifests" -type d | sed -e "s#${BASE_PATH}/##" -e "s#/_manifests##"`; do
-#     echo "# Processing repository: $repository"
-#     ls ${BASE_PATH}/${repository}/_manifests/tags/*/current/link >/dev/null 2>&1 || { echo "ERROR: Invalid repository. No tags/*/current/link files found"; echo; continue; }
-#     test -d "${BASE_PATH}/${repository}/_manifests/revisions/sha256" || { echo "ERROR: Invalid repository. revisions/sha256 dir not found"; echo; continue; }
-
-#     for tag_hash in ${BASE_PATH}/${repository}/_manifests/tags/*/current/link; do 
-#         cat "${tag_hash}" | cut -d':' -f2; 
-#     done > "${used_hashes}"
-    
-#     echo "# Removing revisions of $repository:"
-#     ls -t ${BASE_PATH}/${repository}/_manifests/revisions/sha256 | fgrep -vf "${used_hashes}" | tail -n+${KEEP_LAST_IMAGES} | tee ${marked_hashes}
-#     if test -s $marked_hashes ; then
-#       if [ ${DRY_RUN} -ne 1 ]; then
-#           cat ${marked_hashes} | sed "s#^#${BASE_PATH}/${repository}/_manifests/revisions/sha256/#" | xargs rm -rf && found="true"
-#       fi
-#     else
-#       echo "Nothing to delete."
-#     fi
-#     echo
-# done
-# rm ${used_hashes}
-# rm ${marked_hashes}
-#
-#
-#
